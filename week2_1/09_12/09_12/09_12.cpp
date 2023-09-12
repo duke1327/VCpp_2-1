b1271414b61dbff95d38cb0106f60cf59c2ea273
duke1327 <dr9766@naver.com>
2023-09-12 11:08:49 +0900

    2주차 수업

---
+++ week2_1/09_12/09_12/09_12.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+#define ESC 27
+
+int main()
+{
+    char c;
+    cout << "     202007025\n";
+    cout << "     김지민님의\n";
+    cout << "     Application\n";   
+
+    while (1) {
+        c = _getch();
+        if (c == ESC) {
+            break;
+        }
+
+        cout << "command>";
+
+        if (c == 'p') {
+            cout << " p\n";
+            cout << "나는 과제를 겁나 잘하는 학생이다.\n";
+        }
+        else if (c == 'f') {
+            cout << " f\n";
+            cout << "나는 과제가 너무 즐겁다.\n";
+        }
+        else if (c == 'j') {
+            cout << " j\n";
+            cout << "교수님 과제 더 내주세요.\n";
+        }
+        else {
+            cout << "\n다시 입력해주세요.\n";
+        }
+    }
+
+    return 0;
+}
