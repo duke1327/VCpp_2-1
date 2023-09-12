369b4beea7d42ba0d5ffdd4105fdff8e35ed2221
duke1327 <dr9766@naver.com>
2023-09-12 11:46:02 +0900

    2주차 과제 수정

--- week2_2/09_12-2/09_12-2/09_12-2.cpp
+++ week2_2/09_12-2/09_12-2/09_12-2.cpp
@@ -9,27 +9,28 @@ int main()
     cout << "202007025 김지민 2주차 과제 \n\n";
 
     while (1) {
+        cout << "command>";
+
         c = _getch();
+
         if (c == ESC) {
             break;
         }
 
-        cout << "command>";
-
         if (c == 'q') {
-            cout << " q\n";
+            cout << "q\n";
             cout << "qwer 화이팅!\n";
         }
         else if (c == 'w') {
-            cout << " w\n";
+            cout << "w\n";
             cout << "과제 너무 좋다.\n";
         }
         else if (c == 'e') {
-            cout << " e\n";
+            cout << "e\n";
             cout << "담주부턴 과제량 3배다.\n";
         }
         else if (c == 'r') {
-            cout << " r\n";
+            cout << "r\n";
             cout << "행복합니다.\n";
         }
         else {
