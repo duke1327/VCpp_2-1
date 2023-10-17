f7fdcb2931d449ddff8e6db1b97b085e4099a17a
duke1327 <dr9766@naver.com>
2023-10-17 11:51:32 +0900

    7주차수업

---
+++ week7/week7.cpp
@@ -0,0 +1,151 @@
+﻿#include <windows.h>
+
+// 전역 변수
+HINSTANCE hInst;
+HWND hWnd;
+HWND hButton1, hButton2, hButton3;
+HBRUSH hBrush_red = CreateSolidBrush(RGB(255, 0, 0)); // 빨간색 박스
+HBRUSH hBrush_blue = CreateSolidBrush(RGB(0, 0, 255)); // 파랑색 박스
+HBRUSH hBrush_green = CreateSolidBrush(RGB(0, 255, 0)); // 초록색 박스
+POINT startPoint = { 0 };
+POINT endPoint = { 0 };
+
+// 박스를 나타내는 변수
+bool isBoxVisible = false;
+bool isEllipseVisible = false;
+bool isMouseLButtonPressed = false;
+
+// 박스 그리기 함수
+void DrawBox(HWND hWnd, HDC hdc) {
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+
+    if (isBoxVisible) {
+        // 박스 그리기
+
+        SelectObject(hdc, hBrush_red);
+        Rectangle(hdc, 80, 20, 140, 80);
+        DeleteObject(hBrush_red);
+    }
+}
+
+void DrawEllipse(HWND hWnd, HDC hdc, POINT startPoint, POINT endPoint) {
+    if (isEllipseVisible) {
+        // 타원 그리기
+        SelectObject(hdc, hBrush_blue);
+        Ellipse(hdc, startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+        DeleteObject(hBrush_blue);
+    }
+}
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_LBUTTONDOWN:
+    {
+        startPoint.x = LOWORD(lParam);
+        startPoint.y = HIWORD(lParam);
+    }
+    break;
+    case WM_LBUTTONUP:
+    {
+        endPoint.x = LOWORD(lParam);
+        endPoint.y = HIWORD(lParam);
+    }
+    break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) {
+            // 첫 번째 버튼 클릭
+            isBoxVisible = true;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        else if (LOWORD(wParam) == 2) {
+            // 두 번째 버튼 클릭
+            isBoxVisible = false;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        else if (LOWORD(wParam) == 3) {
+            // 두 번째 버튼 클릭
+            isEllipseVisible = true;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        DrawBox(hWnd, hdc);
+        DrawEllipse(hWnd, hdc, startPoint, endPoint);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 윈도우 초기화 함수
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+    hInst = hInstance;
+
+    hWnd = CreateWindow(
+        L"ButtonWindowClass", L"Win32 Button Example", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd) {
+        return FALSE;
+    }
+
+    hButton1 = CreateWindow(
+        L"BUTTON", L"Add Box", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        20, 20, 60, 60, hWnd, (HMENU)1, hInstance, NULL);
+
+    hButton2 = CreateWindow(
+        L"BUTTON", L"Remove Box", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        20, 200, 60, 60, hWnd, (HMENU)2, hInstance, NULL);
+
+    hButton3 = CreateWindow(
+        L"BUTTON", L"ellipse", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        20, 350, 60, 60, hWnd, (HMENU)3, hInstance, NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+// 프로그램 진입점
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"ButtonWindowClass";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        return 1;
+    }
+
+    if (!InitInstance(hInstance, nCmdShow)) {
+        return 1;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
