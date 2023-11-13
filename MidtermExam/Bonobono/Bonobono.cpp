94f41e57092b4476696c25090508a8f382790f61
duke1327 <dr9766@naver.com>
2023-11-13 19:51:49 +0900

    프로젝트명 오류 수정

--- MidtermExam/Bonobono/Bonobono.cpp
+++ MidtermExam/Bonobono/Bonobono.cpp
@@ -1,4 +1,4 @@
-﻿#include "YuhanCG.h"
+﻿#include <windows.h>
 
 // 전역 변수
 HINSTANCE hInst;
@@ -14,18 +14,41 @@ POINT movedStartPoint = { 0 }; //  이동된 시작점
 POINT movedEndPoint = { 0 }; // 이동된 끝점
 POINT distance = { 0 }; // 마우스 이동거리
 POINT distanceLine = { 0 }; // 원 크기 조절 이동거리
+POINT ryanDistance = { 0 }; // 라이언 얼굴 길이
+// 보노보노 수염 지정 좌표
+POINT bonoMustacheStart[4] = { {320,295},{323,332},{478,281},{480,325} };
+POINT bonoMustacheEnd[4] = { {359,302},{365,313},{435,301},{437,316} };
+POINT bonoEyeLineStart[4] = { {260,230},{260,270},{530,230},{530,270} };
+POINT bonoEyeLineEnd[4] = { {290,250},{290,250},{500,250},{500,250} };
+// 라이언 눈썹 좌표
+POINT ryanEyeBrowStart1 = { 0 };
+POINT ryanEyeBrowEnd1 = { 0 };
+POINT ryanEyeBrowStart2 = { 0 };
+POINT ryanEyeBrowEnd2 = { 0 };
 // 색깔 브러쉬
 HBRUSH boxColor = CreateSolidBrush(RGB(115, 250, 255));
 HBRUSH circleColor = CreateSolidBrush(RGB(185, 85, 255));
+HBRUSH bonoBody = CreateSolidBrush(RGB(127, 200, 255));
+HBRUSH bonoMouse = CreateSolidBrush(RGB(255, 150, 255));
+HBRUSH whiteColor = CreateSolidBrush(RGB(255, 255, 255));
+HBRUSH blackColor = CreateSolidBrush(RGB(0, 0, 0));
+HBRUSH ryanColor = CreateSolidBrush(RGB(255, 200, 15));
 // 그리기 영역
 RECT drawArea;
 // 사각형 미리 정의
 RECT rectangle;
-// 유한 객체 생성
-YuhanCG yuhan;
+// 라이언 좌표
+RECT ryanEar1;
+RECT ryanEar2;
+RECT ryanFace;
+RECT ryanNose1;
+RECT ryanNose2;
+RECT ryanEye1;
+RECT ryanEye2;
 
 bool isMouseLButtonPressed = false;
 bool isMouseRButtonPressed = false;
+bool bonoEyeClose = false;
 
 //모드 설정
 int mode = 0;
@@ -34,7 +57,6 @@ int marginSize = 8;
 int boxMargin = 16;
 int buttonWidth = 140;
 int buttonHeight = 64;
-int bonoEyeClose = 0;
 
 // 윈도우 프로시저
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -194,6 +216,49 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         else if (mode == 4 && isMouseLButtonPressed) {
             endPoint.x = LOWORD(lParam);
             endPoint.y = HIWORD(lParam);
+            distance.x = endPoint.x - startPoint.x;
+            distance.y = endPoint.y - startPoint.y;
+
+            ryanEar1 = { startPoint.x,
+                startPoint.y,
+                startPoint.x + (distance.x / 5 * 2),
+                startPoint.y + (distance.y / 5 * 2) };
+            ryanEar2 = { endPoint.x - (distance.x / 5 * 2),
+                startPoint.y,
+                endPoint.x,
+                startPoint.y + (distance.y / 5 * 2) };
+            ryanFace = { startPoint.x + (distance.x / 20),
+                startPoint.y + (distance.y / 10),
+                endPoint.x - (distance.x / 20),
+                endPoint.y };
+
+            ryanDistance.x = ryanFace.right - ryanFace.left;
+            ryanDistance.y = ryanFace.bottom - ryanFace.top;
+
+            ryanNose1 = { ryanFace.left + (ryanDistance.x / 12 * 5),
+                ryanFace.top + (ryanDistance.y / 2),
+                ryanFace.left + (ryanDistance.x / 2),
+                ryanFace.top + (ryanDistance.y / 5 * 3) };
+            ryanNose2 = { ryanFace.left + (ryanDistance.x / 2),
+                ryanFace.top + (ryanDistance.y / 2),
+                ryanFace.right - (ryanDistance.x / 12 * 5),
+                ryanFace.top + (ryanDistance.y / 5 * 3) };
+            ryanEye1 = { ryanFace.left + (ryanDistance.x / 4),
+                ryanFace.top + (ryanDistance.y / 8 * 3),
+                ryanFace.left + (ryanDistance.x / 16 * 5),
+                ryanFace.top + (ryanDistance.y / 16 * 7) };
+            ryanEye2 = { ryanFace.right - (ryanDistance.x / 4),
+                ryanFace.top + (ryanDistance.y / 8 * 3),
+                ryanFace.right - (ryanDistance.x / 16 * 5),
+                ryanFace.top + (ryanDistance.y / 16 * 7) };
+            ryanEyeBrowStart1 = { ryanFace.left + (ryanDistance.x / 16 * 3),
+                ryanFace.top + (ryanDistance.y / 4) };
+            ryanEyeBrowEnd1 = { ryanFace.left + (ryanDistance.x / 8 * 3),
+                ryanFace.top + (ryanDistance.y / 4) };
+            ryanEyeBrowStart2 = { ryanFace.right - (ryanDistance.x / 8 * 3),
+                ryanFace.top + (ryanDistance.y / 4) };
+            ryanEyeBrowEnd2 = { ryanFace.right - (ryanDistance.x / 16 * 3),
+                ryanFace.top + (ryanDistance.y / 4) };
             InvalidateRect(hWnd, NULL, TRUE);
         }
         else if (mode == 5 && isMouseLButtonPressed) {
@@ -204,15 +269,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     }
 
     case WM_KEYDOWN:
-        if (wParam == VK_SPACE && mode == 3) {
-            bonoEyeClose = 1;
+        if (wParam == VK_SPACE) {
+            bonoEyeClose = true;
             InvalidateRect(hWnd, NULL, TRUE);
         }
         break;
 
     case WM_KEYUP:
         if (bonoEyeClose) {
-            bonoEyeClose = 0;
+            bonoEyeClose = false;
             InvalidateRect(hWnd, NULL, TRUE);
         }
         break;
@@ -253,10 +318,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             Ellipse(hdc, startPoint.x, startPoint.y, endPointSaved.x, endPointSaved.y);
         }
         else if (mode == 3) {
-            yuhan.DrawBonobono(hWnd, hdc, bonoEyeClose);
+            SelectObject(hdc, bonoBody);
+            Ellipse(hdc, 205, 100, 595, 460);
+            SelectObject(hdc, bonoMouse);
+            Ellipse(hdc, 375, 290, 425, 385);
+            SelectObject(hdc, whiteColor);
+            Ellipse(hdc, 340, 280, 400, 330);
+            Ellipse(hdc, 400, 280, 460, 330);
+            SelectObject(hdc, blackColor);
+            Ellipse(hdc, 380, 270, 420, 305);
+            for (int i = 0; i < 4; i++) {
+                MoveToEx(hdc, bonoMustacheStart[i].x, bonoMustacheStart[i].y, NULL);
+                LineTo(hdc, bonoMustacheEnd[i].x, bonoMustacheEnd[i].y);
+            }
+            if (!bonoEyeClose) { // 눈 부분만 따로 그리기
+                Ellipse(hdc, 265, 245, 280, 270);
+                Ellipse(hdc, 522, 245, 537, 270);
+                SelectObject(hdc, whiteColor);
+                Ellipse(hdc, 268, 250, 276, 265);
+                Ellipse(hdc, 525, 250, 533, 265);
+            }
+            else { // 스페이스바 눌렀을 시 작동
+                for (int i = 0; i < 4; i++) {
+                    MoveToEx(hdc, bonoEyeLineStart[i].x, bonoEyeLineStart[i].y, NULL);
+                    LineTo(hdc, bonoEyeLineEnd[i].x, bonoEyeLineEnd[i].y);
+                }
+            }
         }
         else if (mode == 4) {
-            yuhan.DrawRyan(hWnd, hdc, startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+            SelectObject(hdc, ryanColor);
+            Ellipse(hdc, ryanEar1.left, ryanEar1.top, ryanEar1.right, ryanEar1.bottom);
+            Ellipse(hdc, ryanEar2.left, ryanEar2.top, ryanEar2.right, ryanEar2.bottom);
+            Ellipse(hdc, ryanFace.left, ryanFace.top, ryanFace.right, ryanFace.bottom);
+            SelectObject(hdc, whiteColor);
+            Ellipse(hdc, ryanNose1.left, ryanNose1.top, ryanNose1.right, ryanNose1.bottom);
+            Ellipse(hdc, ryanNose2.left, ryanNose2.top, ryanNose2.right, ryanNose2.bottom);
+            SelectObject(hdc, blackColor);
+            Ellipse(hdc, ryanEye1.left, ryanEye1.top, ryanEye1.right, ryanEye1.bottom);
+            Ellipse(hdc, ryanEye2.left, ryanEye2.top, ryanEye2.right, ryanEye2.bottom);
+            for (int i = 0; i < 3; i++) {
+                MoveToEx(hdc, ryanEyeBrowStart1.x, ryanEyeBrowStart1.y + (i * 2), NULL);
+                LineTo(hdc, ryanEyeBrowEnd1.x, ryanEyeBrowEnd1.y + (i * 2));
+                MoveToEx(hdc, ryanEyeBrowStart2.x, ryanEyeBrowStart2.y + (i * 2), NULL);
+                LineTo(hdc, ryanEyeBrowEnd2.x, ryanEyeBrowEnd2.y + (i * 2));
+            }
         }
         else if (mode == 5) {
             startPoint2.x = startPoint.x + 30;
@@ -308,6 +413,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                       {endPoint.x, endPoint.y},
                       {endPoint.x, startPoint.y}}
             };
+            /*
+            if (startPoint.y > endPoint.y) {
+                POINT temp[4];
+                for (int i = 0; i < 4; i++) {
+                    temp[i] = vertices[1][i];
+                    vertices[1][i] = vertices[4][i];
+                    vertices[4][i] = temp[i];
+                }
+                for (int j = 0; j < 6; j++) {
+                    Polygon(hdc, vertices[j], 4);
+                }
+            }
+            else {
+                for (int i = 0; i < 6; i++) {
+                    Polygon(hdc, vertices[i], 4);
+                }
+            }
+            */
             for (int i = 0; i < 6; i++) {
                 Polygon(hdc, vertices[i], 4);
             }
